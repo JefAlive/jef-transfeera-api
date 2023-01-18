94f1e3a08a5d557f1c96c449789fc23daab273c7
Colarites Kersten <coke.digital.mkt@gmail.com>
2023-01-18 14:39:48 -0300

    delete recipient unit test

--- src/useCases/Recipients/DeleteRecipient/DeleteRecipientUseCase.spec.ts
+++ src/useCases/Recipients/DeleteRecipient/DeleteRecipientUseCase.spec.ts
@@ -1,5 +1,42 @@
+import { describe, expect, test, jest } from '@jest/globals';
+import { IRecipientsRepository } from 'src/repositories/IRecipientsRepository';
+import { DeleteRecipientUseCase } from './DeleteRecipientUseCase';
+
 describe('delete recipient use case', () => {
-  test('delete recipient', async () => {
-    
+  let repositoryMock: jest.Mocked<IRecipientsRepository>;
+  let useCase: DeleteRecipientUseCase;
+
+  beforeEach(() => {
+    repositoryMock = {
+      find: jest.fn(),
+      list: jest.fn(),
+      count: jest.fn(),
+      save: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+      deleteMany: jest.fn()
+    };
+    useCase = new DeleteRecipientUseCase(repositoryMock);
+  })
+
+  test('deletes a recipient', async () => {
+    repositoryMock.delete = jest.fn(async (id: string) => {});
+
+    await useCase.execute({
+      id: '0750f176-e301-47c5-b592-414b90275872'
+    });
+
+    await expect(repositoryMock.delete).toHaveBeenCalledWith('0750f176-e301-47c5-b592-414b90275872');
   });
+
+  test('requires valid uuid', async () => {
+    const call = async () => {
+      await useCase.execute({
+        id: 'invalid-uuid-here'
+      });
+    };
+
+    await expect(call).rejects.toThrowError('invalid uuid');
+    expect(repositoryMock.delete).not.toHaveBeenCalledWith();
+  })
 });
--- src/useCases/Recipients/DeleteRecipient/DeleteRecipientUseCase.ts
+++ src/useCases/Recipients/DeleteRecipient/DeleteRecipientUseCase.ts
@@ -1,5 +1,7 @@
 import { IRecipientsRepository } from "src/repositories/IRecipientsRepository";
 import { IDeleteRecipientRequestDTO } from "./DeleteRecipientDTO";
+import { version as uuidVersion } from 'uuid';
+import { validate as uuidValidate } from 'uuid';
 
 export class DeleteRecipientUseCase {
   constructor(
@@ -7,6 +9,13 @@ export class DeleteRecipientUseCase {
   ) {}
 
   async execute(data: IDeleteRecipientRequestDTO): Promise<any> {
+    this.validateIsUuidV4(data.id);
     return await this.recipientsRepository.delete(data.id);
+  }
+
+  private validateIsUuidV4(uuid: string) {
+    if (!uuidValidate(uuid) || uuidVersion(uuid) !== 4) {
+      throw Error('invalid uuid');
+    }
   }
 }
